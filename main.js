42e1439d3fea16ebe20aa24ec8e8f9d5a871e76f
jokay03J-V2 <jmagro@edenschool.fr>
2023-06-28 15:56:46 +0200

    update main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+"use strict";
 import "./style.css";
 
 const forecastContainer = document.querySelector("#forecasts");
@@ -8,6 +9,7 @@ const forecastData = [
     icon: "sleet.png",
     temp_max: "14",
     temp_min: "10",
+    alt: "neige fondu",
   },
   {
     title_day: "lun",
@@ -15,6 +17,39 @@ const forecastData = [
     icon: "chance-storm.png",
     temp_max: "14",
     temp_min: "10",
+    alt: "chance qu'un orage tombe",
+  },
+  {
+    title_day: "lun",
+    title_date: "20",
+    icon: "snow.png",
+    temp_max: "14",
+    temp_min: "10",
+    alt: "neige",
+  },
+  {
+    title_day: "lun",
+    title_date: "20",
+    icon: "sunny.png",
+    temp_max: "14",
+    temp_min: "10",
+    alt: "soleil",
+  },
+  {
+    title_day: "lun",
+    title_date: "20",
+    icon: "thunder-storm.png",
+    temp_max: "14",
+    temp_min: "10",
+    alt: "tempête",
+  },
+  {
+    title_day: "lun",
+    title_date: "20",
+    icon: "wind.png",
+    temp_max: "14",
+    temp_min: "10",
+    alt: "vent",
   },
   {
     title_day: "lun",
@@ -22,6 +57,23 @@ const forecastData = [
     icon: "chance-storm.png",
     temp_max: "14",
     temp_min: "10",
+    alt: "chance qu'un orage tombe",
+  },
+  {
+    title_day: "lun",
+    title_date: "20",
+    icon: "fog.png",
+    temp_max: "14",
+    temp_min: "10",
+    alt: "brouillard",
+  },
+  {
+    title_day: "lun",
+    title_date: "20",
+    icon: "hazy.png",
+    temp_max: "14",
+    temp_min: "10",
+    alt: "brumeux",
   },
   {
     title_day: "lun",
@@ -29,48 +81,7 @@ const forecastData = [
     icon: "chance-storm.png",
     temp_max: "14",
     temp_min: "10",
-  },
-  {
-    title_day: "lun",
-    title_date: "20",
-    icon: "chance-storm.png",
-    temp_max: "14",
-    temp_min: "10",
-  },
-  {
-    title_day: "lun",
-    title_date: "20",
-    icon: "chance-storm.png",
-    temp_max: "14",
-    temp_min: "10",
-  },
-  {
-    title_day: "lun",
-    title_date: "20",
-    icon: "chance-storm.png",
-    temp_max: "14",
-    temp_min: "10",
-  },
-  {
-    title_day: "lun",
-    title_date: "20",
-    icon: "chance-storm.png",
-    temp_max: "14",
-    temp_min: "10",
-  },
-  {
-    title_day: "lun",
-    title_date: "20",
-    icon: "chance-storm.png",
-    temp_max: "14",
-    temp_min: "10",
-  },
-  {
-    title_day: "lun",
-    title_date: "20",
-    icon: "chance-storm.png",
-    temp_max: "14",
-    temp_min: "10",
+    alt: "chance qu'un orage tombe",
   },
 ];
 
@@ -78,7 +89,7 @@ for (const forecastItem of forecastData) {
   forecastContainer.innerHTML += `        <li class="forecast-item">
           <article>
             <h3><span>${forecastItem.title_day}</span><span>${forecastItem.title_date}</span></h3>
-            <img src="./img/${forecastItem.icon}" alt="blizzard">
+            <img src="./img/${forecastItem.icon}" alt="${forecastItem.alt}">
             <div class="forecast-temps"><span class="forecast-max forecast-temp">${forecastItem.temp_max}</span><span class="forecast-divider">|</span><span class="forecast-min forecast-temp">${forecastItem.temp_min}</span></div>
           </article>
         </li>`;
